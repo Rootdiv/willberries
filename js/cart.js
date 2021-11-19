5536ea9baa45b082ae258b919201d9df42a7b4dc
Rootdiv <rootdiv@mail.ru>
2021-11-19 15:40:19 +0300

    feat: Getting elements from a page. Event Handling

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+const cart = () => {
+  'use strict'
+
+  const cartBtn = document.querySelector('.button-cart');
+  const cartModal = document.getElementById('modal-cart');
+  const closeBtn = cartModal.querySelector('.modal-close');
+
+  cartBtn.addEventListener('click', () => {
+    cartModal.style.display = 'flex';
+  })
+
+  closeBtn.addEventListener('click', () => {
+    cartModal.removeAttribute('style');
+  })
+}
+
+cart();
---
+++ js/search.js
@@ -0,0 +1,16 @@
+const search = () => {
+  'use strict'
+
+  const input = document.querySelector('.search-block > input');
+  const searchBtn = document.querySelector('.search-block > button');
+
+
+  // input.addEventListener('input', event => {
+  //   console.log(event.target.value);
+  // })
+  searchBtn.addEventListener('click', () => {
+    console.log('input.value: ', input.value);
+  })
+}
+
+search();
