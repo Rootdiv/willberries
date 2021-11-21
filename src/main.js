d3b75847661627a81c782df4a49aa73b20c15886
Rootdiv <rootdiv@mail.ru>
2021-11-21 21:16:34 +0300

    feat: closing modal window and smooth scroll

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import card from './modules/cart';
 import search from './modules/search';
 import getGoods from './modules/getGoods';
+import smoothScroll from './modules/smoothScroll';
 
 card();
 search();
 getGoods();
+smoothScroll();
--- src/modules/cart.js
+++ src/modules/cart.js
@@ -5,6 +5,19 @@ const cart = () => {
   const cartModal = document.getElementById('modal-cart');
   const closeBtn = cartModal.querySelector('.modal-close');
 
+  cartModal.addEventListener('click', event => {
+    const target = event.target;
+    if (!target.closest('.modal')) {
+      cartModal.removeAttribute('style');
+    }
+  });
+
+  window.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      cartModal.removeAttribute('style');
+    }
+  });
+
   cartBtn.addEventListener('click', () => {
     cartModal.style.display = 'flex';
   });
